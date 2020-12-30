ea147112197b657e3a33ad8c82489cb594655801
sungareyjan <69305408+sungareyjan@users.noreply.github.com>
2020-12-29 18:31:13 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE HTML>
+<html><head>
+<meta charset="utf-8">
+<title>TEST</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="main.css?version=1" >
+</head><body>
+<img class="img-banner" src="img/banner1.jpg" alt="main banner">
+<div class="wrapper">
+<article class="img-info">
+<h2>HACKDOG</h2>
+<p>hakdog. [slang] a nonsensical meme expression, usually in response to "ha" to make fun of whatever their responding to; hotdog.</p>
+</article>
+<img class="img-hacker" src="img/hacker.png" alt="hacker">
+</div>
+
+</body></html>              
